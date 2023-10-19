ca17d70f5387e776fd6c588a4596bd63a6a298f3
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-19 14:55:25 -0600

    Interfaces - First Commit

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,6 +1,27 @@
-fun main() {
-    val person: Person = Student(32, "Nate", "Ebel")
-    person.age
 
-    person.printName()
+interface StringProvider {
+
+    val placeholder: String
+
+    fun getString(id: Int): String = id.toString()
+}
+
+interface ResourceProvider : StringProvider {
+    fun getDimension(id: Int): Long = id.toLong()
+}
+
+class DefaultResourceProvider : ResourceProvider {
+
+    override val placeholder: String = "<placeholder>"
+}
+
+fun main() {
+    val someVariable: Any? = null
+    if (someVariable is StringProvider) {
+        println("The variable was a StringProvider")
+    }
+
+    var stringProvider: StringProvider = DefaultResourceProvider()
+    println(stringProvider.getString(5))
+    println(stringProvider.placeholder)
 }
