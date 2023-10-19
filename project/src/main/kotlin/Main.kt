2b16f7e0d05c6edfb38ff4c0715a9ff24789e88d
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-19 15:02:04 -0600

    Data Classes - First Commit

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 fun main() {
-    val person: Person = Student(32, "Nate", "Ebel")
-    person.age
+    val task1 = Task("a1", "Add analytics")
+    val task2 = Task("a1", "Add analytics")
 
-    person.printName()
+    println(task1.name)
+    println(task2.name)
+
+    val task2Copy = task2.copy(id = "c3")
+
+    val (id, name) = task1
+
+    if (task2Copy == task2) {
+        println("Tasks are the same")
+    } else {
+        println("Tasks are different")
+    }
 }
