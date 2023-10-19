6ae4a95cc071259b5445a78cac6fbaefbe364061
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-19 14:58:46 -0600

    Enum - First Commit

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
-fun main() {
-    val person: Person = Student(32, "Nate", "Ebel")
-    person.age
 
-    person.printName()
+
+fun main() {
+    val direction: Direction = Direction.WEST
+
+    //println(direction.name)
+    //println(direction.ordinal)
+
+    //val parsedDirection = Direction.valueOf("kotlin")
+    //println(direction.name)
+
+    //val apiValue = Direction.values()[6]
+    //println(apiValue.name)
+
+    val color = HighlightColor.BLUE
+    println(color.value)
 }
