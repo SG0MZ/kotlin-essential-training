32b71c6a5f8f495cce06cd42c3998b3f8a138c0e
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-20 08:12:07 -0600

    Extension Properties - First Commit

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,6 +1,19 @@
-fun main() {
-    val person: Person = Student(32, "Nate", "Ebel")
-    person.age
+val Int.isEven: Boolean
+    get() = this.mod(2) == 0
 
-    person.printName()
+fun main() {
+//    millisForHours(4)
+//    millisForHours(10)
+
+    4.millisForHours()
+    10.millisForHours()
+
+    "[abc]+".toRegex()
+
+//    println(8.isEven)
+//    println(7.isEven)
+
+    5.log()
+    "sample string".log()
+    listOf(1, 2, 3).log()
 }
